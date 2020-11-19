37536849e94cf6f70a03afc6257dcc8fbce2e0cd
Z3n1th <31189482+zhenia92@users.noreply.github.com>
2020-11-19 21:23:11 +0000

    Update ip_port_range.py

--- python/ip_port_range.py
+++ python/ip_port_range.py
@@ -1,3 +1,5 @@
+#coding=utf-8
+
 ip = list()
 port = list()
 
@@ -7,8 +9,18 @@ for x in range(1,1025):
     f = open('portList.txt','a')
     f.write(str(x)+'\n')
     f.close()
-    
-    
+
+# Debug Ports
+# print(port)
+
+# Another Way to do
+
+#f = open('portList2.txt','a')
+#portList = [(str(x)+'\n') for x in range(1,1025)]
+#f.writelines(portList)
+#f.close()
+
+
 
 for y in range(0,256):
     ip_pre = '192.168.1.' + str(y)
@@ -17,3 +29,16 @@ for y in range(0,256):
     f = open('ipList.txt','a')
     f.write(ip_pre +'\n')
     f.close()
+
+# Debug Ports
+# print(ip)
+
+# Another Way to do
+
+#pre_ip = '192.168.1.'
+#f = open('ipList2.txt','a')
+#IpList = [(pre_ip+str(x)+'\n') for x in range(0,256)]
+#f.writelines(IpList)
+#f.close()
+
+
